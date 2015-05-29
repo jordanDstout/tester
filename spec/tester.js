60030a50abdc8f0c9e46b3d3ccb86eab4d67ba5c
jordanDstout <jordanstout2014@gmail.com>
2015-05-29 15:25:49 -0700

    boilerplate

---
+++ spec/tester.js
@@ -0,0 +1,26 @@
+var path = require('path');
+var expect = require('chai').expect;
+
+var tester = require(path.join(__dirname, '..', './tester1.js'));
+describe('tester()', function () {
+  'use strict';
+
+  it('exists', function () {
+    expect(tester.is6()).to.be.a('function');
+
+  });
+
+  it('does something', function () {
+    expect(true).to.equal(true);
+  });
+
+  it('does something else', function () {
+    expect(false).to.equal(false);
+  });
+  
+  it('is 6', function (){
+    var y=tester.is6(6)
+    expect(y).to.equal(true);
+  });
+  // Add more assertions here
+});
